4720fca8aa19dd3aa1caeb42e8a7665a01bd7b40
KSchalau <97081280+KSchalau@users.noreply.github.com>
2022-12-02 19:45:55 -0800

    added basic key interaction

---
+++ Assets/Scripts/KeyMovement.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+source: https://www.youtube.com/watch?v=qkBlPiUNslI
+*/
+
+public class KeyMovement : MonoBehaviour {
+
+    private bool isFollowing;
+    public float followSpeed;
+    private Transform followTarget;
+
+    void Start() {
+        //isFollowing = true;
+    }
+
+    void Update() {
+        if (isFollowing) {
+            transform.position = Vector3.Lerp(transform.position, followTarget.position, followSpeed * Time.deltaTime);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D col) {
+        if (!isFollowing) {
+            ProtagonistBehaviour thePlayer = FindObjectOfType<ProtagonistBehaviour>();
+            followTarget = thePlayer.transform;
+            isFollowing = true;
+        }
+    }
+}
